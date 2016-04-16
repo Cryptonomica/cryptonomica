bc6ce63dde85a8cf36078cd8e76a6f4fc33efbd8
Viktor <ageyev@gmail.com>
2016-04-16 20:17:43 +0300

    add net.cryptonomica.servlets.IPNhandlerServlet

---
+++ src/main/java/net/cryptonomica/servlets/IPNhandlerServlet.java
@@ -0,0 +1,94 @@
+package net.cryptonomica.servlets;
+
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.logging.Logger;
+
+/**
+ *
+ */
+public class IPNhandlerServlet extends HttpServlet {
+
+    /* --- Logger: */
+    private final static Logger LOG = Logger.getLogger(IPNhandlerServlet.class.getName());
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        /* --- send email with payment details: */
+        final Queue queue = QueueFactory.getDefaultQueue();
+        queue.add(
+                TaskOptions.Builder
+                        .withUrl(
+                                "/_ah/SendGridServlet")
+                        .param("email",
+                                "admin@cryptonomica.net"
+                        )
+                        .param("messageSubject",
+                                "New payment info"
+                        )
+                        .param("messageText",
+                                new Gson().toJson(req)
+                        )
+        );
+
+        /* --- log request headers: */
+        Enumeration headerNames = req.getHeaderNames();
+        LOG.warning("[Headers names start]: ");
+        while (headerNames.hasMoreElements()) {
+            Object nextElement = headerNames.nextElement();
+            LOG.warning(nextElement.toString());
+            LOG.warning(new Gson().toJson(nextElement));
+        }
+        LOG.warning("[Headers names end]// ");
+
+        /* --- read and log request body: */
+        // http://stackoverflow.com/questions/14525982/getting-request-payload-from-post-request-in-java-servlet
+        // http://stackoverflow.com/questions/3831680/httpservletrequest-get-json-post-data
+        // see also:
+        // http://stackoverflow.com/questions/1548782/retrieving-json-object-literal-from-httpservletrequest/15109169#15109169
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader reader = req.getReader(); // <<---
+        String line;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        String reqBody = stringBuilder.toString();
+        LOG.warning("[request body]:");
+        LOG.warning(reqBody);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+        Enumeration headerNames = req.getHeaderNames();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter pw = resp.getWriter(); //get the stream to write the data
+        pw.println("request headers: ");
+        while (headerNames.hasMoreElements()) {
+            pw.println(headerNames.nextElement().toString());
+        }
+        pw.println("~~~~~~~~~~~~");
+        pw.println("request parameters:");
+        pw.println(
+                gson.toJson(
+                        req.getParameterMap()
+                )
+        );
+        pw.close(); //closing the stream
+    }
+}
